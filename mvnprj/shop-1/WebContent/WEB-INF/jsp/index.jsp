5055e21292a9c322c2673178e96b4f9b777b6af6
홈 <ytw9699@naver.com>
2019-04-16 22:52:41 +0900

    샵1

--- mvnprj/shop-1/WebContent/WEB-INF/jsp/index.jsp
+++ mvnprj/shop-1/WebContent/WEB-INF/jsp/index.jsp
@@ -7,22 +7,22 @@
 <title>상품 목록 화면</title>
 </head>
 <body>
-<div align="center" class="body">
-<h2>상품 목록 화면</h2>
-<table border="1">
-	<tr class="header">
-		<th align="center" width="80">상품ID</th>
-		<th align="center" width="320">상품명</th>
-		<th align="center" width="100">가격</th>
-	</tr>
-	<c:forEach var="item" items="${itemList}">
-		<tr class="record">
-			<td align="center">${item.itemId}</td>
-			<td align="left">${item.itemName}</td>
-			<td align="right">${item.price}원</td>
-		</tr>
-	</c:forEach>
-</table>
-</div>
+	<div align="center" class="body">
+		<h2>상품 목록 화면</h2>
+			<table border="1">
+				<tr class="header">
+					<th align="center" width="80">상품ID</th>
+					<th align="center" width="320">상품명</th>
+					<th align="center" width="100">가격</th>
+				</tr>
+				<c:forEach var="item" items="${itemList}">
+					<tr class="record">
+						<td align="center">${item.itemId}</td>
+						<td align="left">${item.itemName}</td>
+						<td align="right">${item.price}원</td>
+					</tr>
+				</c:forEach>
+			</table>
+	</div>
 </body>
 </html>
--- mvnprj/shop-1/src/dao/ItemDaoImpl.java
+++ mvnprj/shop-1/src/dao/ItemDaoImpl.java
@@ -1,30 +1,26 @@
 package dao;
-
-import java.util.List;
-
-import javax.sql.DataSource;
-
-import logic.Item;
-
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
-import org.springframework.jdbc.core.RowMapper;
-import org.springframework.jdbc.core.simple.SimpleJdbcTemplate;
-//db쪽 작업
-public class ItemDaoImpl implements ItemDao {//dao가 여러개 되야 이 전체적인 구조를 이해할수있다 지금은 이렇게 한개.. 
-
-	private SimpleJdbcTemplate template;
-	//모든 메소드 기능 합쳐놓은 SimpleJdbcTemplate , 3가지경우다쓰는 템플릿 이라 이거쓰자
-	public void setDataSource(DataSource dataSource) {
-		this.template = new SimpleJdbcTemplate(dataSource);//이 객체가 있어야 디비단작업함
+	import java.util.List;
+	import javax.sql.DataSource;
+	import logic.Item;
+	import org.springframework.jdbc.core.BeanPropertyRowMapper;
+	import org.springframework.jdbc.core.RowMapper;
+	import org.springframework.jdbc.core.simple.SimpleJdbcTemplate;
+	//db쪽 작업
+	public class ItemDaoImpl implements ItemDao {//dao가 여러개 되야 이 전체적인 구조를 이해할수있다 지금은 이렇게 한개.. 
+	
+		private SimpleJdbcTemplate template;
+		//모든 메소드 기능 합쳐놓은 SimpleJdbcTemplate , 3가지경우다쓰는 템플릿 이라 이거쓰자
+		public void setDataSource(DataSource dataSource) {
+			this.template = new SimpleJdbcTemplate(dataSource);//이 객체가 있어야 디비단작업함
+		}
+		private static final String SELECT_ALL = //상수로 쿼리문 정의
+	"SELECT item_id, item_name, price, description, picture_url FROM item order by item_id";// item테이블에서 
+	//item_id기준 오름차순 정렬
+	public List<Item> findAll() {//ItemDao에서 오버라이드
+		RowMapper<Item> mapper = new BeanPropertyRowMapper<Item>(Item.class);
+		//생성자에 자바빈이름만 넣어주면 SELECT문으로 꺼내온데이터를 자바빈에 자동으로 셋팅하는듯
+		return this.template.query(ItemDaoImpl.SELECT_ALL, mapper);//mapper객체를 통해 자바빈에 꺼내온거 설정
 	}
-	private static final String SELECT_ALL = //상수로 쿼리문 정의
-"SELECT item_id, item_name, price, description, picture_url FROM item order by item_id";// item테이블에서 
-//item_id기준 오름차순 정렬
-public List<Item> findAll() {//ItemDao에서 오버라이드
-RowMapper<Item> mapper = new BeanPropertyRowMapper<Item>(Item.class);
-//생성자에 자바빈이름만 넣어주면 SELECT문으로 꺼내온데이터를 자바빈에 자동으로 셋팅하는듯
-return this.template.query(ItemDaoImpl.SELECT_ALL, mapper);//mapper객체를 통해 자바빈에 꺼내온거 설정
-}
 }
 //1.이 과정(RowMapper<Item> mapper = new BeanPropertyRowMapper<Item>(Item.class);)을
 //2.ItemDaoImpl.SELECT_ALL, mapper);의 mapper이름으로 넣어줘서 아래 3. 코드 를 생략
