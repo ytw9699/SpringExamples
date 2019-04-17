39a78a3133d2f7af7e24cafdb85e9c3a0d9f1041
홈 <ytw9699@naver.com>
2019-04-17 14:03:50 +0900

    샵3

--- mvnprj/shop-3/src/controller/LoginFormController.java
+++ mvnprj/shop-3/src/controller/LoginFormController.java
@@ -1,14 +1,14 @@
 package controller;
-import logic.Shop;
-import logic.User;
-import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.Validator;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
+	import logic.Shop;
+	import logic.User;
+	import org.springframework.dao.EmptyResultDataAccessException;
+	import org.springframework.stereotype.Controller;
+	import org.springframework.validation.BindingResult;
+	import org.springframework.validation.Validator;
+	import org.springframework.web.bind.annotation.ModelAttribute;
+	import org.springframework.web.bind.annotation.RequestMapping;
+	import org.springframework.web.bind.annotation.RequestMethod;
+	import org.springframework.web.servlet.ModelAndView;
 //http://localhost:8080/shop-3/login.html
 @Controller
 public class LoginFormController {
@@ -35,31 +35,31 @@ public class LoginFormController {
 		return new User();
 	}
 
-@RequestMapping(method = RequestMethod.POST)
-public ModelAndView onSubmit(User user, BindingResult bindingResult) {
-
-	this.loginValidator.validate(user, bindingResult);
-
-	ModelAndView modelAndView = new ModelAndView();
-	if (bindingResult.hasErrors()) {
-		modelAndView.getModel().putAll(bindingResult.getModel());//질문
-		return modelAndView;
-	}
-	try {
-	// 유저 정보 검색
-	User loginUser = this.shopService.getUserByUserIdAndPassword(user.getUserId(), user.getPassword());
-
-		// 유저가 있을 때
-		modelAndView.setViewName("loginSuccess");
-		modelAndView.addObject("loginUser", loginUser);
-		return modelAndView;
-	} catch (EmptyResultDataAccessException e) {
-		// 유저가 없을 때
-		bindingResult.reject("error.login.user");
-		modelAndView.getModel().putAll(bindingResult.getModel());//질문
-		return modelAndView;
-	}
-	}
+	@RequestMapping(method = RequestMethod.POST)
+	public ModelAndView onSubmit(User user, BindingResult bindingResult) {
+	
+		this.loginValidator.validate(user, bindingResult);
+	
+		ModelAndView modelAndView = new ModelAndView();
+		if (bindingResult.hasErrors()) {
+			modelAndView.getModel().putAll(bindingResult.getModel());//질문
+			return modelAndView;
+		}
+		try {
+		// 유저 정보 검색
+		User loginUser = this.shopService.getUserByUserIdAndPassword(user.getUserId(), user.getPassword());
+	
+			// 유저가 있을 때
+			modelAndView.setViewName("loginSuccess");
+			modelAndView.addObject("loginUser", loginUser);
+			return modelAndView;
+		} catch (EmptyResultDataAccessException e) {
+			// 유저가 없을 때
+			bindingResult.reject("error.login.user");
+			modelAndView.getModel().putAll(bindingResult.getModel());//질문
+			return modelAndView;
+		}
+		}
 }
 /*@RequestMapping(method = RequestMethod.POST)
 public String submit(@ModelAttribute("login") LoginCommand loginCommand,//login이름 동일
