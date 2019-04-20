3c20b1d91b7b0b4eca6bd0317aa5e2606e0d9123
홈 <ytw9699@naver.com>
2019-04-20 19:30:15 +0900

    챕터6 aop

--- mvnprj/spring4-chap06/src/main/java/net/madvirus/spring4/chap06/aop/Profiler.java
+++ mvnprj/spring4-chap06/src/main/java/net/madvirus/spring4/chap06/aop/Profiler.java
@@ -2,7 +2,7 @@ package net.madvirus.spring4.chap06.aop;
 
 import org.aspectj.lang.ProceedingJoinPoint;
 
-public class Profiler {
+public class Profiler {//공통모듈임//로그찍어보는것
 
 public Object trace(ProceedingJoinPoint joinPoint) throws Throwable {//ProceedingJoinPoint을 첫마라미터로 꼭 넣어야함..
 	String signatureString = joinPoint.getSignature().toShortString();//호출되는 메소드에 대한 정보를 구한다
