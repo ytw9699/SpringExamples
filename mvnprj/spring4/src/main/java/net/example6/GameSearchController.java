9809a451565751a1e12e2087bc75b87bbaa6e6d9
홈 <ytw9699@naver.com>
2019-04-22 12:42:31 +0900

    exampl6

--- mvnprj/spring4/src/main/java/net/example6/GameSearchController.java
+++ mvnprj/spring4/src/main/java/net/example6/GameSearchController.java
@@ -1,15 +1,12 @@
 package net.example6;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
-
+	import java.util.ArrayList;
+	import java.util.List;
+	import org.springframework.beans.factory.annotation.Autowired;
+	import org.springframework.stereotype.Controller;
+	import org.springframework.web.bind.annotation.ExceptionHandler;
+	import org.springframework.web.bind.annotation.ModelAttribute;
+	import org.springframework.web.bind.annotation.RequestMapping;
+	import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class GameSearchController {
 	@Autowired
