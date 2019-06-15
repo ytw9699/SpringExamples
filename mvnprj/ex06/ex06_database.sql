2e24f6b5b982dd161c75c79857f710a2a2fd29df
home_champ <ytw9699@naver.com>
2019-06-16 00:48:13 +0900

    패스워드 인코더 문제해결

--- mvnprj/ex06/ex06_database.sql
+++ mvnprj/ex06/ex06_database.sql
@@ -9,6 +9,8 @@ create table authorities (
 	constraint fk_authorities_users foreign key(username) references users(username)
 );
 
+create unique index ix_auth_username on authorities (username,authority);
+
 insert into users(username, password ) values ( 'user00' , 'pw00');
 insert into users(username, password ) values ( 'member00' , 'pw00');
 insert into users(username, password ) values ( 'admin00' , 'pw00');
@@ -18,4 +20,23 @@ insert into authorities(username, authority) values ('member00','ROLE_MANAGER');
 insert into authorities(username, authority) values ('admin00','ROLE_MANAGER');
 insert into authorities(username, authority) values ('admin00','ROLE_ADMIN');
 
+
+select * from users;
+
+select * from authorities order by authority;
+
+create table tbl_member(
+      userid varchar2(50) not null primary key,
+      userpw varchar2(100) not null,
+      username varchar2(100) not null,
+      regdate date default sysdate, 
+      updatedate date default sysdate,
+      enabled char(1) default '1');
+
+
+create table tbl_member_auth (
+     userid varchar2(50) not null,
+     auth varchar2(50) not null,
+     constraint fk_member_auth foreign key(userid) references tbl_member(userid)
+);
 
