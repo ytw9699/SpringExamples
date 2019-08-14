d52f7f6e3cb8c4a3f6250d3caa2423d2244d7b47
home_champ <ytw9699@naver.com>
2019-08-14 20:24:13 +0900

    임포트정리

--- mvnprj/ex05/src/main/java/org/zerock/controller/UploadController.java
+++ mvnprj/ex05/src/main/java/org/zerock/controller/UploadController.java
@@ -1,36 +1,33 @@
 package org.zerock.controller;
-
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.net.URLDecoder;
-import java.net.URLEncoder;
-import java.nio.file.Files;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.UUID;
-
-import org.springframework.core.io.FileSystemResource;
-import org.springframework.core.io.Resource;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.util.FileCopyUtils;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.multipart.MultipartFile;
-import org.zerock.domain.AttachFileDTO;
-
-import lombok.extern.log4j.Log4j;
-import net.coobird.thumbnailator.Thumbnailator;
+	import java.io.File;
+	import java.io.FileOutputStream;
+	import java.io.IOException;
+	import java.io.UnsupportedEncodingException;
+	import java.net.URLDecoder;
+	import java.net.URLEncoder;
+	import java.nio.file.Files;
+	import java.text.SimpleDateFormat;
+	import java.util.ArrayList;
+	import java.util.Date;
+	import java.util.List;
+	import java.util.UUID;
+	import org.springframework.core.io.FileSystemResource;
+	import org.springframework.core.io.Resource;
+	import org.springframework.http.HttpHeaders;
+	import org.springframework.http.HttpStatus;
+	import org.springframework.http.MediaType;
+	import org.springframework.http.ResponseEntity;
+	import org.springframework.stereotype.Controller;
+	import org.springframework.ui.Model;
+	import org.springframework.util.FileCopyUtils;
+	import org.springframework.web.bind.annotation.GetMapping;
+	import org.springframework.web.bind.annotation.PostMapping;
+	import org.springframework.web.bind.annotation.RequestHeader;
+	import org.springframework.web.bind.annotation.ResponseBody;
+	import org.springframework.web.multipart.MultipartFile;
+	import org.zerock.domain.AttachFileDTO;
+	import lombok.extern.log4j.Log4j;
+	import net.coobird.thumbnailator.Thumbnailator;
 
 @Controller
 @Log4j
