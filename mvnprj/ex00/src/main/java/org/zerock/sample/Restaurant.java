3486a0ee9989fdfe122c0078b82759149f15ce9b
home_champ <ytw9699@naver.com>
2020-01-24 23:00:32 +0900

    주석수정

--- mvnprj/ex00/src/main/java/org/zerock/sample/Restaurant.java
+++ mvnprj/ex00/src/main/java/org/zerock/sample/Restaurant.java
@@ -4,16 +4,16 @@ package org.zerock.sample;
 	import lombok.Data;
 	import lombok.Setter;
 	
-@Component 
-@Data 
-public class Restaurant {
+@Component //@Component는 스프링에게 해당 클래스가 스프링에서 관리해야 하는 대상임을 표시하는 어노테이션 그러면 스프링은 이클래스 인스턴스를 생성해줌
+@Data //@Data는 setter를 생성하는 기능과 생성자，toString() 등을 자동으로 생성
+	  // @ToString, @EqualsAndHashCode, @Getter/©Setter, @RequiredArgsCtonstructor를 모두결합한 형태로 자주 사용되는 모든 메서드들을 한 번에 생성할 수 있다는 장점
+public class Restaurant {//Restaurant 객체는 Chef 타입의 객체를 필요로 하는 상황
 	 @Setter(onMethod_ = @Autowired)
 	 private Chef chef;
+	 /*
+		 @setter는 자동으로 setChef()메서드를 컴파일 시 생성함
+		 @Setter에서 사용된 onMethod 속성은 생성되는 setChef()에 @Autowired 어노테이션을 추가하도록함
+		 스프링은 @Autowired 어노테이션붙어서 chef객체를 주입해줌
+	 */
 } 
-/*@Component는 스프링에게 해당 클래스가 스프링에서 관리해야 하는 대상임을 표시하는 어노테이션
-,그러면 스프링은 이클래스 인스턴스를 생성해줌
-@Data는 setter를 생성하는 기능과 생성자，toString() 등을 자동으로 생성, @ToString,
-@EqualsAndHashCode, @Getter/©Setter, @RequiredArgsCtonstructor를 모두결합한 형태로 자주 사용되는 모든 메서드들을 한 번에 생성할 수 있다는 장점
-@setter는 자동으로 setChef( )메서드를 컴파일 시 생성함
-@Setter에서 사용된 onMethod 속성은 생성되는 setChef()에 @Autowired 어노테이션을 추가하도록함
-스프링은 @Autowired 어노테이션붙어서 chef객체를 주입해줌*/
+
