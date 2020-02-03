8b757c379b248d18ea1fe1b4f3c30d2d404f3660
home_champ <ytw9699@naver.com>
2020-02-04 05:11:17 +0900

    //직접 생성방식의 단점

--- mvnprj/ex00/src/main/java/org/zerock/sample/SampleHotel4.java
+++ mvnprj/ex00/src/main/java/org/zerock/sample/SampleHotel4.java
@@ -7,7 +7,7 @@ package org.zerock.sample;
 public class SampleHotel4 {
 	
 	private Chef chef = new maleChef();//직접 생성방식
-	//private Chef chef = new femaleChef();//직접 생성방식
+	//private Chef chef = new femaleChef();//직접 생성방식의 단점
 	
 } 
 
