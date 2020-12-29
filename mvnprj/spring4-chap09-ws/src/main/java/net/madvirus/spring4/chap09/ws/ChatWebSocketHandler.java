d698b93838c9f53d638f05a47d99c33625f0a18b
home_champ <ytw9699@naver.com>
2020-12-29 16:11:15 +0900

    예시목록

--- mvnprj/spring4-chap09-ws/src/main/java/net/madvirus/spring4/chap09/ws/ChatWebSocketHandler.java
+++ mvnprj/spring4-chap09-ws/src/main/java/net/madvirus/spring4/chap09/ws/ChatWebSocketHandler.java
@@ -14,26 +14,31 @@ public class ChatWebSocketHandler extends TextWebSocketHandler {
 	private Map<String, WebSocketSession> users = new ConcurrentHashMap<>();
 
 	@Override
-	public void afterConnectionEstablished(
-			WebSocketSession session) throws Exception {
+	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+		
 		log(session.getId() + " 연결 됨");
+		
 		users.put(session.getId(), session);
 	}
 
 	@Override
-	public void afterConnectionClosed(
-			WebSocketSession session, CloseStatus status) throws Exception {
+	public void afterConnectionClosed( WebSocketSession session, CloseStatus status) throws Exception {
+		
 		log(session.getId() + " 연결 종료됨");
+		
 		users.remove(session.getId());
 	}
 
 	@Override
-	protected void handleTextMessage(
-			WebSocketSession session, TextMessage message) throws Exception {
+	protected void handleTextMessage( WebSocketSession session, TextMessage message) throws Exception {
+		
 		log(session.getId() + "로부터 메시지 수신: " + message.getPayload());
+		
 		for (WebSocketSession s : users.values()) {
+			
 			s.sendMessage(message);
 			log(s.getId() + "에 메시지 발송: " + message.getPayload());
+			
 		}
 	}
 
