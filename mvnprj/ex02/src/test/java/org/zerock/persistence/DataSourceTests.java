539b8173aac1dad8ca93616081eb3dbd687d9ecf
champ_home <ytw9699@naver.com>
2022-03-16 00:03:42 +0900

    테스트 파일 주석

--- mvnprj/ex02/src/test/java/org/zerock/persistence/DataSourceTests.java
+++ mvnprj/ex02/src/test/java/org/zerock/persistence/DataSourceTests.java
@@ -26,8 +26,7 @@ public class DataSourceTests {
   private SqlSessionFactory sqlSessionFactory;
   
   @Test
-  public void testConnection() {//testConnection()을 실행해 보면 내부적으로 HikariCP가 시작되고，종료되는 로그를 확인
-    
+  public void testConnection() {
     try (Connection con = dataSource.getConnection()){
 
       log.info(con);      
@@ -36,6 +35,14 @@ public class DataSourceTests {
       fail(e.getMessage());
     }
   }
+  /* 스프링에 빈으로 등록된 dataSource를 이용해서 Connection을
+	제대로 처리할 수 있는지를 확인해 보는 용도, 
+	내부으로 hikariConfig가 시작되고, 종료되는 로그를 확인가능
+	INFO : org.springframework.context.support.GenericApplicationContext - 
+	Closing org.springframework.context.support.GenericApplicationContext@45820e51: 
+	startup date [Mon Apr 20 22:17:45 KST 2020]; root of context hierarchy
+	INFO : com.zaxxer.hikari.pool.HikariPool - HikariCP pool HikariPool-0 is shutting down.
+testConnection()을 실행해 보면 내부적으로 HikariCP가 시작되고，종료되는 로그를 확인*/
   
   @Test
   public void testMyBatis() {//SqlSessionFactoryBean을 이용해서 SqlSession을 사용해 보고 Connection객체 까지 얻어내는 테스트
@@ -44,8 +51,8 @@ public class DataSourceTests {
        Connection con = session.getConnection();
       ) {
 
-      log.info(session);
-      log.info(con);
+      log.info(session);// org.apache.ibatis.session.defaults.DefaultSqlSession@9fec931
+      log.info(con);//HikariConnectionProxy(2097989776) wrapping net.sf.log4jdbc.sql.jdbcapi.ConnectionSpy@49293b43
 
     } catch (Exception e) {
       fail(e.getMessage());
