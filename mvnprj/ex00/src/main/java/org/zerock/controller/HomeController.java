eeeb5986d19fd35b17d6367747108096089eb86c
champ_home <ytw9699@naver.com>
2022-03-31 14:25:09 +0900

    어노테이션 추가

--- mvnprj/ex00/src/main/java/org/zerock/controller/HomeController.java
+++ mvnprj/ex00/src/main/java/org/zerock/controller/HomeController.java
@@ -1,20 +1,20 @@
 package org.zerock.controller;
-
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+	import java.text.DateFormat;
+	import java.util.Date;
+	import java.util.Locale;
+	import org.slf4j.Logger;
+	import org.slf4j.LoggerFactory;
+	import org.springframework.stereotype.Controller;
+	import org.springframework.ui.Model;
+	import org.springframework.web.bind.annotation.RequestMapping;
+	import org.springframework.web.bind.annotation.RequestMethod;
+	import lombok.ToString;
 
 /**
  * Handles requests for the application home page.
  */
 @Controller
+@ToString
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
@@ -32,6 +32,8 @@ public class HomeController {
 		String formattedDate = dateFormat.format(date);
 		
 		model.addAttribute("serverTime", formattedDate );
+		
+		System.out.println(this);
 		
 		return "home";
 	}
