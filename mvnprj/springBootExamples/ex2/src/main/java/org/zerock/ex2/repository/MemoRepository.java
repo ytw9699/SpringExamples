200a7024b0804359853b5b218f3f9bf6bcec5dfc
champ_home <ytw9699@naver.com>
2022-07-10 12:23:23 +0900

    chore : ex2 예제 삭제

--- mvnprj/springBootExamples/ex2/src/main/java/org/zerock/ex2/repository/MemoRepository.java
+++
@@ -1,7 +0,0 @@
-package org.zerock.ex2.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.zerock.ex2.entity.Memo;
-
-public interface MemoRepository extends JpaRepository<Memo, Long> {
-}
